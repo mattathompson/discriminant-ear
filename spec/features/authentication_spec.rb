d8296b6c95f46f2ab2b447a922855e7914300163
Matthew Thompson <matt@mattathompson.org>
2016-03-24 00:22:21 -0400

    Adds specs testing the registraion process w/ feature specs

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -13,3 +13,18 @@ describe "the signin process", :type => :feature do
     expect(page).to have_content 'Signed in successfully. The Discriminant Ear'
   end
 end
+
+describe "the registration process", :type => :feature do
+
+
+  it "signs me up" do
+    visit '/users/sign_up'
+    within("form") do
+      fill_in 'Email', :with => 'user@example.com'
+      fill_in 'Password', :with => 'password'
+      fill_in "Password confirmation", :with => "password"
+    end
+    click_button 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully. The Discriminant Ear'
+  end
+end
