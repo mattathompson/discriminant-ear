e7af4eeab3f166b46de38442a8a87e5fa4f260c9
Matthew Thompson <matt@mattathompson.org>
2016-04-08 00:09:07 -0400

    Update the users controller/model to handle the oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   resources :user, only: [:show, :index] do
-    
+
   end
 
   root 'pages#landing'
