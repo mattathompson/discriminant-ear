c87a30a83fe5fdfb29cff869e4f9495ee7c7f4b3
Matthew Thompson <matt@mattathompson.org>
2016-04-09 14:42:35 -0400

    Working GoogleAuthentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,22 @@ class User < ActiveRecord::Base
 
 
   def self.from_omniauth(access_token)
-    binding.pry
     data = access_token.info
-    user = User.where(:email => data["email"]).first
+    binding.pry
+    user = User.where(:email => data.email).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data["name"],
-    #        email: data["email"],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
+    unless user
+      names = data.name.split(" ")
+      user = User.create(
+         first_name: names[0],
+         last_name: names[1],
+         email: data.email,
+         uid: access_token.uid,
+         provider: access_token.provider,
+         password: Devise.friendly_token[0,20]
+      )
+    end
     user
   end
 
