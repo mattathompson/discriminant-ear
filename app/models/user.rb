e1a656a32132e06e7f4ac7cb27a74a777f42bfc4
Matthew Thompson <matt@mattathompson.org>
2016-04-09 12:21:18 -0400

    Add columns to users to store uid and provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,28 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
 
-   def self.from_omniauth(access_token)
-     data = access_token.info
-     user = User.where(:email => data["email"]).first
 
-     # Uncomment the section below if you want users to be created if they don't exist
-     # unless user
-     #     user = User.create(name: data["name"],
-     #        email: data["email"],
-     #        password: Devise.friendly_token[0,20]
-     #     )
-     # end
-     user
+  def self.from_omniauth(access_token)
+    binding.pry
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    # unless user
+    #     user = User.create(name: data["name"],
+    #        email: data["email"],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    user
   end
 
+  # def self.from_omniauth(auth)
+  #    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #      user.provider = auth.provider
+  #      user.uid = auth.uid
+  #      user.email = auth.info.email
+  #      user.password = Devise.friendly_token[0,20]
+  #    end
+  # end
 end
